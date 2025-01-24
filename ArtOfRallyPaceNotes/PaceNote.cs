895942f6c04e69c378d2769349fc5a4087a04123
Moon Dragon <42640027+MoonDragon-MD@users.noreply.github.com>
2025-01-24 20:17:22 +0100

    Add files via upload

--- ArtOfRallyPaceNotes/PaceNote.cs
+++ ArtOfRallyPaceNotes/PaceNote.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using JetBrains.Annotations;
 using ArtOfRallyPaceNotes.Settings;
 using UnityEngine;
@@ -14,57 +13,26 @@ namespace ArtOfRallyPaceNotes
         [CanBeNull] public static string[] PaceNoteConfig = null;
 
         [CanBeNull] public static Dictionary<string, Texture2D> Textures = null;
-
-        private static float LastTimestamp;
-
-        private const float DisplayTime = 2;
-
-        private const float FadeTime = 0.5f;
-
-        private static string PaceNoteKey;
+        [CanBeNull] public static Dictionary<string, AudioClip> AudioClips = null;
+        private static AudioSource audioSource;
 
         public static void Draw(UnityModManager.ModEntry modEntry)
         {
-            var newKey = PaceNoteConfig?[PaceNoteManager.CurrentWaypointIndex];
-            if (newKey != null && newKey != PaceNoteKey)
-            {
-                PaceNoteKey = newKey;
-                LastTimestamp = Time.time;
-            }
-
-            var time = Time.time - LastTimestamp;
-            var fade = Mathf.Clamp(time > FadeTime
-                ? 1f - (time + FadeTime - DisplayTime) / FadeTime
-                : time / FadeTime , 0, 1
-            );
-            if (time > FadeTime && fade <= 0)
-            {
-                PaceNoteKey = null;
-            }
+            var paceNote = PaceNoteConfig?[PaceNoteManager.CurrentWaypoint];
 
             if (Main.Settings.ShowCurrentWaypoint)
             {
                 GUI.Label(
                     new Rect(0, 0, 200, 200),
-                    $"Waypoint {PaceNoteManager.CurrentWaypointIndex}\n"
-                    + $"∠{PaceNoteManager.Angles?[PaceNoteManager.CurrentWaypointIndex]:F}° "
-                    + $"∑{PaceNoteManager.MeanAngles?[PaceNoteManager.CurrentWaypointIndex]:F}° "
-                    + $"@ {Main.Settings.DistanceTolerance:F}m\n"
-                    + $"↕{PaceNoteManager.Elevations?[PaceNoteManager.CurrentWaypointIndex]:F}m "
-                    + $"∑{PaceNoteManager.MeanElevations?[PaceNoteManager.CurrentWaypointIndex]:F}m "
-                    + $"@ {Main.Settings.DistanceTolerance:F}m\n"
-                    + $"↔{PaceNoteManager.Distances?[PaceNoteManager.CurrentWaypointIndex]:F}m\n"
-                    + $"Pace Note: {PaceNoteKey}\n"
-                    + $"Fade: {fade}"
+                    $"Waypoint {PaceNoteManager.CurrentWaypoint}\nPace Note: {paceNote}"
                 );
             }
 
-            if (PaceNoteKey == null || Textures?.ContainsKey(PaceNoteKey) != true) return;
+            if (paceNote == null || Textures?.ContainsKey(paceNote) != true) return;
+            var texture = Textures![paceNote];
 
-            var texture = Textures![PaceNoteKey];
             var halfSize = Main.Settings.PaceNoteSize / 2;
 
-            GUI.color = new Color(1, 1, 1, fade);
             GUI.DrawTexture(
                 new Rect(
                     Screen.width * Main.Settings.PositionX - halfSize,
@@ -76,5 +44,27 @@ namespace ArtOfRallyPaceNotes
                 ScaleMode.ScaleToFit
             );
         }
+        public static void PlayAudioNote(string noteKey)
+        {
+            if (!Main.Settings.EnableAudio || AudioClips == null || !AudioClips.ContainsKey(noteKey))
+                return;
+            
+            if (audioSource == null)
+                audioSource = /* create and configure AudioSource */;
+            
+            audioSource.clip = AudioClips[noteKey];
+            audioSource.volume = Main.Settings.AudioVolume;
+            audioSource.Play();
+        }
+    
+        public static void Draw(UnityModManager.ModEntry modEntry)
+        {
+            var newKey = PaceNoteConfig?[PaceNoteManager.CurrentWaypointIndex];
+            if (newKey != null && newKey != PaceNoteKey)
+            {
+                PaceNoteKey = newKey;
+                LastTimestamp = Time.time;
+                PlayAudioNote(newKey); // Reproduce the audio when the note changes
+            }
     }
 }
